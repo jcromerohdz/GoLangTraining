7d1fa7873c24c5d610671a7150fa5b9a0ef2228d
jcromerohdz <jcromerohdz@gmail.com>
2017-08-01 08:07:49 -0700

    Packages in go

---
+++ Packages/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jcromerohdz/GoLangTraining/Packages/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
+	fmt.Println(stringutil.MyName)
+
+}
